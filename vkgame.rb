1a8cc088f6866b7ba54e8f954ac9ec8939d05f42
eremite <eremite@gmail.com>
2009-03-07 20:21:10 -0700

    Added username and password.

--- vkgame.rb
+++ vkgame.rb
@@ -19,18 +19,37 @@ ActiveRecord::Base.establish_connection({
 
 class VirtualKingdomsGame < Shoes
   show_log
+  @@email = ''
+  @@password = ''
 
   def index
+    title "Virtual Kingdoms: the Game"
+    e, pw = '', ''
     stack do
-      title "VKGAME"
-      download "#{VK_SERVER_URL}/pages/download?id=#{GAME_ID}", :save => DBPATH do |r|
-        if (200..300).include?(r.response.headers['Status'].to_i)
-          visit '/game'
-        else
-          para File.read(DBPATH)
+      flow do
+        para "Email:"
+        e = edit_line
+      end
+      flow do
+        para "Password:"
+        pw = edit_line :secret => true
+      end
+      flow do
+        button "Let the Adventure Begin", :width => 300 do
+          para "Downloading... please be patient"
+          @@email = CGI.escape(e.text)
+          @@password = CGI.escape(pw.text)
+          params = "id=#{GAME_ID}&email=#{@@email}&password=#{@@password}"
+          download "#{VK_SERVER_URL}/pages/download?#{params}", :save => DBPATH do |r|
+            if (200..300).include?(r.response.headers['Status'].to_i)
+              visit '/game'
+            else
+              alert File.read(DBPATH)
+              exit
+            end
+          end
         end
       end
-      para "Now downloading, please be patient"
     end
   end
 
@@ -42,7 +61,8 @@ class VirtualKingdomsGame < Shoes
       para Game.all.map {|g| g.name}.join(' ')
       button "Quit" do
         para "saving..."
-        c = Curl::Easy.new("#{VK_SERVER_URL}/pages/upload?id=#{GAME_ID}")
+        params = "id=#{GAME_ID}&email=#{@@email}&password=#{@@password}"
+        c = Curl::Easy.new("#{VK_SERVER_URL}/pages/upload?#{params}")
         c.multipart_form_post = true
         c.http_post(Curl::PostField.file('game[data]', DBPATH))
         if (200..300).include?(c.response_code)
