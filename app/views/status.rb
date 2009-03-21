cab6dc7e3847ff5693e1b549d14d6258389c4ecc
eremite <eremite@gmail.com>
2009-03-21 15:59:03 -0600

    Forgot to add status file.

---
+++ app/views/status.rb
@@ -0,0 +1,25 @@
+module StatusView
+
+  def show_status
+    keys = %w(left right up down q)
+    @statusa = para "Available keys: #{keys.to_sentence}"
+    # button "Quit" do
+    #   para "saving..."
+    # params = "id=#{GAME_ID}&email=#{@@email}&password=#{@@password}"
+    # c = Curl::Easy.new("#{VK_SERVER_URL}/pages/upload?#{params}")
+    # c.multipart_form_post = true
+    # c.http_post(Curl::PostField.file('game[data]', DBPATH))
+    # if (200..300).include?(c.response_code)
+    #   File.delete(DBPATH)
+    # exit
+    # else
+    #   para "Error"
+    # end
+    # end
+  end
+
+  def update_status(message = '')
+    @statusa.text = message
+  end
+
+end
